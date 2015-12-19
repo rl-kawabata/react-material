11795c91ee72ffd50f51c19c1d209e7dcfdd2250
川端進悟 <shingo@KsPC.local>
2015-12-19 23:21:59 +0900

    ルートの変なクエリ削除

--- src/jsx/app.jsx
+++ src/jsx/app.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router} from 'react-router';
 import injectTapEventPlugin from 'react-tap-event-plugin';
+import createHistory from 'history/lib/createHashHistory'
 import AppRoutes from './app-routes';
 
 injectTapEventPlugin();
 
 ReactDOM.render(
-  <Router>
+  <Router
+    history={createHistory({queryKey: false})}>
     {AppRoutes}
   </Router>
   ,document.getElementById('app')
